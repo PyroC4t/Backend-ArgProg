34749b6f72403dc7343745ebe824d163638dd29b
PyroC4t <tiburopunk@gmail.com>
2022-08-09 16:15:44 -0300

    enlazando back con front

--- src/main/java/com/portfolioAp/ap/Controller/UserController.java
+++ src/main/java/com/portfolioAp/ap/Controller/UserController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("usuario")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolioap-50c82.web.app")
 public class UserController {
     @Autowired
     ServiceUser serviceUser;
